b0fa99027240a88ad4df5bd904e1a49cc663c409
GrzegorzPerko <grzegorzperko@gmail.com>
2020-02-05 17:25:39 +0100

    added changes after comments of pull request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './assets/logo192.png';
+import React from 'react'
+import logo from './assets/logo192.png'
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <header style={{ textAlign: 'center', marginTop: 20 }}>
         <img src={logo} alt="logo"  />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
